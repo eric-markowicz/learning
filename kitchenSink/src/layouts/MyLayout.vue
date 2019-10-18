dc5988ceb298eb7350d28ef30a24018c3d20fe9e
Eric Markowicz <ericmarkowicz@Erics-MacBook-Pro.local>
2019-10-18 09:54:40 -0400

    fixed sliding bar links

--- kitchenSink/src/layouts/MyLayout.vue
+++ kitchenSink/src/layouts/MyLayout.vue
@@ -23,90 +23,97 @@
       content-class="bg-grey-2"
     >
       <q-list>
-        <q-item clickable tag="a" target="_blank" href="http://10.40.63.203:8080/#">
+        <q-item clickable tag="a" href="http://localhost:8080/#/">
           <q-item-section avatar>
             <q-icon name="home" />
           </q-item-section>
           <q-item-section>
             <q-item-label>Home</q-item-label>
-            <q-item-label caption>http://10.40.63.203:8080/#</q-item-label>
           </q-item-section>
         </q-item>
-        <q-item clickable tag="a" target="_blank" href="http://10.40.63.203:8080/#/editor">
+        <q-item clickable tag="a" href="http://localhost:8080/#/editor">
           <q-item-section avatar>
-            <q-icon name="page" />
+            <q-icon name="edit" />
           </q-item-section>
           <q-item-section>
             <q-item-label>Editor</q-item-label>
-            <q-item-label caption>http://10.40.63.203:8080/#/editor</q-item-label>
           </q-item-section>
         </q-item>
-        <q-item clickable tag="a" target="_blank" href="http://10.40.63.203:8080/#/bluetooth">
+        <q-item clickable tag="a" href="http://localhost:8080/#/bluetooth">
           <q-item-section avatar>
             <q-icon name="bluetooth" />
           </q-item-section>
           <q-item-section>
             <q-item-label>Bluetooth</q-item-label>
-            <q-item-label caption>http://10.40.63.203:8080/#/bluetooth</q-item-label>
           </q-item-section>
         </q-item>
-        <q-item clickable tag="a" target="_blank" href="http://10.40.63.203:8080/#/connection">
+        <q-item clickable tag="a" href="http://localhost:8080/#/connection">
           <q-item-section avatar>
-            <q-icon name="connection" />
+            <q-icon name="wifi" />
           </q-item-section>
           <q-item-section>
             <q-item-label>Connection</q-item-label>
-            <q-item-label caption>http://10.40.63.203:8080/#/connection</q-item-label>
           </q-item-section>
         </q-item>
-        <q-item clickable tag="a" target="_blank" href="http://10.40.63.203:8080/#/fileUpload">
+        <q-item clickable tag="a" href="http://localhost:8080/#/fileUpload">
           <q-item-section avatar>
-            <q-icon name="file" />
+            <q-icon name="cloud_upload" />
           </q-item-section>
           <q-item-section>
             <q-item-label>File Upload</q-item-label>
-            <q-item-label caption>http://10.40.63.203:8080/#/fileUpload</q-item-label>
           </q-item-section>
         </q-item>
-        <q-item clickable tag="a" target="_blank" href="http://10.40.63.203:8080/#/Login">
+        <q-item clickable tag="a" href="http://localhost:8080/#/Login">
           <q-item-section avatar>
-            <q-icon name="Login" />
+            <q-icon name="work" />
           </q-item-section>
           <q-item-section>
             <q-item-label>Login</q-item-label>
-            <q-item-label caption>http://10.40.63.203:8080/#/Login</q-item-label>
           </q-item-section>
         </q-item>
-        <q-item clickable tag="a" target="_blank" href="http://10.40.63.203:8080/#/Register">
+        <q-item clickable tag="a" href="http://localhost:8080/#/Register">
           <q-item-section avatar>
-            <q-icon name="Register" />
+            <q-icon name="notes" />
           </q-item-section>
           <q-item-section>
             <q-item-label>Register</q-item-label>
-            <q-item-label caption>http://10.40.63.203:8080/#/Register</q-item-label>
           </q-item-section>
         </q-item>
-        <q-item clickable tag="a" target="_blank" href="http://10.40.63.203:8080/#/Settings">
+        <q-item clickable tag="a" href="http://localhost:8080/#/Settings">
           <q-item-section avatar>
-            <q-icon name="Settings" />
+            <q-icon name="settings_applications" />
           </q-item-section>
           <q-item-section>
             <q-item-label>Settings</q-item-label>
-            <q-item-label caption>http://10.40.63.203:8080/#/Settings</q-item-label>
           </q-item-section>
         </q-item>
-        <q-item clickable tag="a" target="_blank" href="http://10.40.63.203:8080/#/takePic">
+        <q-item clickable tag="a" href="http://localhost:8080/#/camera">
           <q-item-section avatar>
             <q-icon name="camera" />
           </q-item-section>
           <q-item-section>
             <q-item-label>Camera</q-item-label>
-            <q-item-label caption>http://10.40.63.203:8080/#/takePic</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" href="http://localhost:8080/#/dateTime">
+          <q-item-section avatar>
+            <q-icon name="calendar_today" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Calendar</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" href="http://localhost:8080/#/map">
+          <q-item-section avatar>
+            <q-icon name="map" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Map</q-item-label>
           </q-item-section>
         </q-item>
       </q-list>
     </q-drawer>
-
+<!-- target=_blank causes the page to open in a new tab -->
     <q-page-container>
       <router-view />
     </q-page-container>
